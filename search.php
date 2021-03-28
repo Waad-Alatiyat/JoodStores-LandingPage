c87b7d58c9e1fe26de0a2d8aba9a20dfef177929
Waad-Alatiyat <waad.alatiyat@digitalanalytica.net>
2021-03-28 23:12:41 +0300

    First Push

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package Arya_Multipurpose
+ */
+
+get_header();
+?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php arya_multipurpose_inner_banner(); ?>
+
+			<!-- Start blog-lists section -->
+		    
+		    <!-- End blog-lists section -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ single.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Arya_Multipurpose
+ */
+
+get_header();
+	?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php arya_multipurpose_inner_banner(); ?>
+
+			<!-- Start blog-lists section -->
+		    
+		    <!-- End blog-lists section -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
